416c896a0bc1f1970c53c4d8aaef3e83364e2395
Meir J Cohen <meir84@gmail.com>
2022-11-11 21:36:35 +0200

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/diceSet3/dice-target.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Under Construction...</h1>
       </header>
     </div>
   );
 }
 
 export default App;
+
+//-----
+// UNDER CONSTRUVCTION
+//-----
+
+/* <div className="App">
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <h1>Under Construction...</h1>
+</header>
+</div> */
